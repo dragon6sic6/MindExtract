57e3496767fa15ca4eaa31802d3f5ccbaf5889f6
dragon6sic6 <137377532+dragon6sic6@users.noreply.github.com>
2026-03-12 21:43:27 +0100

    chore: bump version to 1.2.2 (build 3)

--- sign_and_notarize.sh
+++ sign_and_notarize.sh
@@ -30,7 +30,7 @@ fi
 BUNDLE_ID="com.mindact.mindextract"
 
 APP_NAME="MindExtract"
-DMG_NAME="MindExtract-1.2.1-Universal"
+DMG_NAME="MindExtract-1.2.2-Universal"
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
 BUILD_DIR="$PROJECT_DIR/build"
 OUTPUT_DIR="$PROJECT_DIR/dist"
